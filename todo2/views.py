a5dd95f34bd36b86a6b65673c396d2693fc87519
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2024-10-08 19:40:46 +0000

    qwerty

--- todo2/views.py
+++ todo2/views.py
@@ -4,6 +4,7 @@ from typing import List
 from controller import get_todo, get_todos, create_todo, update_todo, delete_todo
 from schemas import TodoCreate, TodoUpdate, TodoResponse
 from database import get_db
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -21,18 +22,19 @@ def read_todo(todo_id: int, db: Session = Depends(get_db)):
 
 @router.post("/todos/", response_model=TodoResponse)
 def create_new_todo(todo: TodoCreate, db: Session = Depends(get_db)):
-    return create_todo(db=db, todo=todo)
+    return JSONResponse(content={"message": "Tarefa criada com sucesso!"}, status_code=200)
 
 @router.put("/todos/{todo_id}", response_model=TodoResponse)
 def update_existing_todo(todo_id: int, todo: TodoUpdate, db: Session = Depends(get_db)):
     updated_todo = update_todo(db=db, todo_id=todo_id, todo=todo)
     if updated_todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
-    return updated_todo
+    return JSONResponse(content={"message": "Tarefa atualizada com sucesso!"}, status_code=200)
 
 @router.delete("/todos/{todo_id}", response_model=TodoResponse)
 def delete_existing_todo(todo_id: int, db: Session = Depends(get_db)):
     deleted_todo = delete_todo(db=db, todo_id=todo_id)
     if deleted_todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
-    return deleted_todo
+    
+    return JSONResponse(content={"message": "Tarefa deletada com sucesso!"}, status_code=200)
